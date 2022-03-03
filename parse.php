7042312a9837fedc21033057574d5e1673d2ddec
MrDalo <dadokralik@gmail.com>
2022-03-03 17:49:52 +0100

    Skusobne odovzdanie 1 projektu

--- parse.php
+++ parse.php
@@ -1,6 +1,14 @@
 <?php
+/**
+ * Project: IPP22
+ * @author Dalibor Kralik, xkrali20
+ * @brief Module which process program arguments
+ * 
+ * @file programArguments.php
+ */
+
 ini_set('display_errors', 'stderr');
-include "./programArguments.php";
+
 
 
 $xml = new XMLWriter();
@@ -45,6 +53,45 @@ const operationCode = [
         ['DPRINT', 'symb'],
         ['BREAK']
     ];
+
+/**
+ * @brief Function which process program arguments
+ * @use call processArgument() in external modules
+ * 
+ */
+function processArgument()
+{
+    global $argc, $argv;
+
+    if($argc == 2)
+    {
+        $argument = $argv[1];
+        if($argument == "--help")
+        {
+            echo("This is Help section of parser.php\n");
+            echo("=====================================\n\n");
+            echo("This program supports just one argument\n\n");
+            echo("      --help      -shows help section of parser.php\n\n\n");
+            echo("=====================================\n\n");
+            echo("The program can by run as:\n");
+            echo "      \$php parser.php [--help]\n\n";
+            echo("=====================================\n\n");
+            exit(0);
+        }
+        else
+        {
+            fwrite(STDERR, "ERROR 10 - unknow argument\n");
+            exit(10);
+        }
+    }
+    else if($argc > 2)
+    {
+        fwrite(STDERR, "ERROR 10 - unexpected combination of arguments\n");
+        exit(10);
+    }
+
+}
+
 
 
 /**
